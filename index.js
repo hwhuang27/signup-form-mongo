517ddede293376afbeb63083e5328aad29e9c422
David Huang <david.huang3214@gmail.com>
2023-04-11 14:10:07 -0700

    draft

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import Users from './model/users.js';
-import moment from 'moment';
 import path from 'path';
-import creds from 'secrets';
 import * as url from 'url';
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-const app = express();
+import mongoose from 'mongoose';
+import moment from 'moment';
+import { creds } from './secrets.js';
+import User from './public/User.js';
+mongoose.connect(creds);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// mongoose.connect(creds);
-// const user = await Users.create({
-//     firstName: 'David',
-//     lastName: 'Huang',
-//     email: 'david.huang3214@gmail.com',
+// const user = await User.create({
+//     firstName: 'David1',
+//     lastName: 'Huang1',
+//     email: 'david1@gmail.com',
 //     password: 'mypassword',
 //     createdAt: moment().format()
 // })
 // console.log(user);
 
-//app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/public', require('./public'))
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/public', require('./public'))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
---
+++ public/User.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    }, 
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+    }
+});
+
+const User = model('User', userSchema);
+export default User;
